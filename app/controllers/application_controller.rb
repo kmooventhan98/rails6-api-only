d91f392d72de7b72b964bfcdd368d0a4b2beb21e
Mooventhan <kmooventhan98@gmail.com>
2022-08-17 00:16:01 +0530

    created book index only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    def render_json(success, message, status)
+        render :json => {:success => success, :response => message}, :status => status
+    end
 end
