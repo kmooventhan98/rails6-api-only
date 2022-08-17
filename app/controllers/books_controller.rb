95e0c1abe6421794b2a2674345824767347896d5
Mooventhan <kmooventhan98@gmail.com>
2022-08-17 22:15:27 +0530

    implemented create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,26 @@ class BooksController < ApplicationController
     # render :json => "hi", :status => 200
     # render_json(true,"hi",200)
     # render :json => "hi"
-    render json: Book.all 
+    render json: Book.all, status: :ok
+  end
+
+  def create
+    # to create without assigning any variabls if assigned any variable we can use .errors inbuilt methods on that variables
+    # Book.create(title: "Java", author: "Balagurusamy")
+    
+    # hardcoded
+    #book = Book.new(title: "Java", author: "Balagurusamy")
+    
+    book = Book.new(title: params[:title], author: params[:author])
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:author)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    validates :author, presence: true
+    validates :title, presence: true
 end
