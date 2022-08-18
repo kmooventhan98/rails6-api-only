3359706586108436dadf5c6e5743e951c4fdb4fe
Mooventhan <kmooventhan98@gmail.com>
2022-08-18 11:49:31 +0530

    added validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    validates :author, presence: true
-    validates :title, presence: true
+    validates :author, presence: true, length: {minimum:3}
+    validates :title, presence: true, length: {mininum:3}
 end
